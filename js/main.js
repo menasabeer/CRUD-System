0112cc866877541b9d09ed61179b79c3f1c2c5c4
menasabeer7@gmail.com <menasabeer7@gmail.com>
2024-12-05 18:37:23 +0200

    update

--- js/main.js
+++ js/main.js
@@ -11,10 +11,12 @@ var alertDisc = document.getElementById("alertDisc");
 var searchInput = document.getElementById("searchInput");
 var products = [];
 var currentIndex = 0;
+
 if (JSON.parse(localStorage.getItem("productList")) != null) {
     products = JSON.parse(localStorage.getItem("productList"));
     displayProduct();
 }
+
 addBtn.onclick = function () {
     if (addBtn.innerHTML == "Add Product") {
         addProduct();
@@ -27,8 +29,9 @@ addBtn.onclick = function () {
     productPriceInput.classList.remove("is-valid");
     productCategoryInput.classList.remove("is-valid");
     productDescriptionInput.classList.remove("is-valid");
-    addBtn.disabled = "true";
+    addBtn.disabled = true;
 };
+
 function addProduct() {
     var product = {
         name: productNameInput.value,
@@ -36,9 +39,10 @@ function addProduct() {
         category: productCategoryInput.value,
         description: productDescriptionInput.value,
     };
-    products.push(product),
-        localStorage.setItem("productList", JSON.stringify(products));
+    products.push(product);
+    localStorage.setItem("productList", JSON.stringify(products));
 }
+
 function displayProduct() {
     var cartona = "";
     for (var i = 0; i < products.length; i++) {
@@ -47,22 +51,25 @@ function displayProduct() {
                     <td>${products[i].price}</td>
                     <td>${products[i].category}</td>
                     <td>${products[i].description}</td>
-                    <td><button onclick='getProductInfo(${i})' class='btn btn-warning'>Upadate</button></td>
+                    <td><button onclick='getProductInfo(${i})' class='btn btn-warning'>Update</button></td>
                     <td><button onclick='deleteProduct(${i})' class='btn btn-danger'>Delete</button></td>
                 </tr>`;
     }
     document.getElementById("tableBody").innerHTML = cartona;
 }
+
 function clearForm() {
     for (var i = 0; i < inputs.length; i++) {
         inputs[i].value = "";
     }
 }
+
 function deleteProduct(index) {
     products.splice(index, 1);
     displayProduct();
     localStorage.setItem("productList", JSON.stringify(products));
 }
+
 searchInput.onkeyup = function () {
     var cartona = "";
     for (var i = 0; i < products.length; i++) {
@@ -76,13 +83,14 @@ searchInput.onkeyup = function () {
                 <td>${products[i].price}</td>
                 <td>${products[i].category}</td>
                 <td>${products[i].description}</td>
-                <td><button onclick='getProductInfo(${i})' class='btn btn-warning'>Upadate</button></td>
+                <td><button onclick='getProductInfo(${i})' class='btn btn-warning'>Update</button></td>
                 <td><button onclick='deleteProduct(${i})' class='btn btn-danger'>Delete</button></td>
             </tr>`;
         }
     }
     document.getElementById("tableBody").innerHTML = cartona;
 };
+
 function getProductInfo(index) {
     currentIndex = index;
     var currentProduct = products[index];
@@ -91,7 +99,24 @@ function getProductInfo(index) {
     productCategoryInput.value = currentProduct.category;
     productDescriptionInput.value = currentProduct.description;
     addBtn.innerHTML = "Update Product";
+    addBtn.disabled = true;
+
+    validateInput(
+        productNameInput,
+        alertName,
+        /^[A-Za-z0-9]{3,20}( [A-Za-z0-9]{3,20})*$/
+    );
+    validateInput(
+        productPriceInput,
+        alertPrice,
+        /^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/
+    );
+    validateInput(productCategoryInput, alertCategory, /^[A-Za-z\s]+$/);
+    validateInput(productDescriptionInput, alertDisc, /^[A-Za-z\s]+$/);
+
+    checkFormValidity();
 }
+
 function updateProduct() {
     var product = {
         name: productNameInput.value,
@@ -101,48 +126,66 @@ function updateProduct() {
     };
     products[currentIndex] = product;
     addBtn.innerHTML = "Add Product";
+    addBtn.disabled = true;
 }
+
 function ifLogic(alert, input) {
     input.classList.add("is-valid");
     input.classList.remove("is-invalid");
     alert.classList.add("d-none");
 }
+
 function elseLogic(alert, input) {
     input.classList.remove("is-valid");
     input.classList.add("is-invalid");
     alert.classList.remove("d-none");
 }
-productNameInput.onkeyup = function () {
-    let nameRejex = /^[A-Za-z0-9]{3,20}( [A-Za-z0-9]{3,20})*$/;
-    if (nameRejex.test(productNameInput.value)) {
-        ifLogic(alertName, productNameInput);
+
+function validateInput(input, alert, regex) {
+    if (regex.test(input.value)) {
+        ifLogic(alert, input);
     } else {
-        elseLogic(alertName, productNameInput);
+        elseLogic(alert, input);
     }
-};
-productPriceInput.onkeyup = function () {
-    let priceRejex = /^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/;
-    if (priceRejex.test(productPriceInput.value)) {
-        ifLogic(alertPrice, productPriceInput);
-    } else {
-        elseLogic(alertPrice, productPriceInput);
-    }
-};
-productCategoryInput.onkeyup = function () {
-    let categoryRejex = /^[A-Za-z\s]+$/;
-    if (categoryRejex.test(productCategoryInput.value)) {
-        ifLogic(alertCategory, productCategoryInput);
-    } else {
-        elseLogic(alertCategory, productCategoryInput);
-    }
-};
-productDescriptionInput.onkeyup = function () {
-    let DescriptionRejex = /^[A-Za-z\s]+$/;
-    if (DescriptionRejex.test(productDescriptionInput.value)) {
-        ifLogic(alertDisc, productDescriptionInput);
+}
+
+function checkFormValidity() {
+    if (
+        productNameInput.classList.contains("is-valid") &&
+        productPriceInput.classList.contains("is-valid") &&
+        productCategoryInput.classList.contains("is-valid") &&
+        productDescriptionInput.classList.contains("is-valid")
+    ) {
         addBtn.removeAttribute("disabled");
     } else {
-        elseLogic(alertDisc, productDescriptionInput);
-        addBtn.disabled = "true";
+        addBtn.disabled = true;
     }
+}
+
+productNameInput.onkeyup = function () {
+    validateInput(
+        productNameInput,
+        alertName,
+        /^[A-Za-z0-9]{3,20}( [A-Za-z0-9]{3,20})*$/
+    );
+    checkFormValidity();
+};
+
+productPriceInput.onkeyup = function () {
+    validateInput(
+        productPriceInput,
+        alertPrice,
+        /^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/
+    );
+    checkFormValidity();
+};
+
+productCategoryInput.onkeyup = function () {
+    validateInput(productCategoryInput, alertCategory, /^[A-Za-z\s]+$/);
+    checkFormValidity();
+};
+
+productDescriptionInput.onkeyup = function () {
+    validateInput(productDescriptionInput, alertDisc, /^[A-Za-z\s]+$/);
+    checkFormValidity();
 };
